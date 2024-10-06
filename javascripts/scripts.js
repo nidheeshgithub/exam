64c54187af65519579679dad44b74eabf34ce53d
nidheeshgithub <85321927+nidheeshgithub@users.noreply.github.com>
2024-10-06 09:33:11 +0530

    Add files via upload

---
+++ javascripts/scripts.js
@@ -0,0 +1,13 @@
+function openContactModal() {
+    document.getElementById('contactModal').classList.add('active');
+    document.getElementById('modalBackdrop').classList.add('active');
+}
+
+function closeContactModal() {
+    document.getElementById('contactModal').classList.remove('active');
+    document.getElementById('modalBackdrop').classList.remove('active');
+}
+
+
+
+
